6e3aebbab55a9a6c7172fe9ffbb9ee9a8279f64c
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-03-27 03:06:11 +0600

    4 items selection and repeate selection worked successfully

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
-const Product = (props) => {
-    const { name, img, price, seller } = props.product
+const Product = ({ product, handleAddToCart }) => {
+    const { name, img, price, seller } = product;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -10,8 +13,9 @@ const Product = (props) => {
                 <p><strong>Price:</strong> ${price}</p>
                 <p><strong> Brand: </strong>{seller}</p>
             </div>
-            <button className='btn-cart'>
-                <p>Add To Cart</p>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                <p className='btn-info'>Add To Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -4,12 +4,31 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
+
+    // 4 item add to Cart
+    const handleAddToCart = (selectedWatch) => {
+        if (cart.length >= 4) {
+            alert('You can select maximum 4 items');
+        } else {
+            let newCart = [];
+            const addedWatch = cart.find(product => product.id === selectedWatch.id);
+            if (addedWatch) {
+                alert("Same item can't be added twice");
+            } else {
+                selectedWatch.quantity = 1;
+                newCart = [...cart, selectedWatch];
+                setCart(newCart);
+            }
+        }
+    }
 
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -17,11 +36,17 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h3>This is Cart Container</h3>
+                <h3>Selected Watch</h3>
+                <div>
+                    {
+                        cart.map(item => <h4 key={item.id}>{item.name}</h4>)
+                    }
+                </div>
             </div>
         </div>
     );
