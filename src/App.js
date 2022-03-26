9239f23ce33e028ecc93116f3b0530a7221e99eb
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-03-26 22:06:53 +0600

    All product are shown in UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './component/Header/Header';
 import Shop from './component/Shop/Shop';
 
+
 function App() {
   return (
     <div className="">
--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import './Product.css'
 const Product = (props) => {
-    console.log(props.product)
+    const { name, img, price, seller } = props.product
     return (
-        <div>
-            <h2>This is product</h2>
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className="product-info">
+                <p className='product-name'>{name}</p>
+                <p><strong>Price:</strong> ${price}</p>
+                <p><strong> Brand: </strong>{seller}</p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add To Cart</p>
+            </button>
         </div>
     );
 };
